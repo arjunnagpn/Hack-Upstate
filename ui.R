618fe3236120a7ec349e77ed2e43d099d1b3ba39
Arjun Nag <arjunnagpn@gmail.com>
2017-04-10 19:56:14 -0400

    Bug fixes

--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@
 # MS in Information Management,
 # CAS in Data Science,
 # Syracuse University - Spring 2017
-# Oct 9, 2016
+# April 9, 2017
 
 # Load required packages to library
 
@@ -41,8 +41,6 @@ shinyUI(
         
         # Display Sidebar Labels
         
-        # menuItem("Summary", tabName = "dashboard", icon = icon("dashboard")),
-        # menuItem("Potholes Graph", tabName = "potholesGraph", icon = icon("bar-chart")),
         menuItem("Recommendation", tabName = "overallsummary", icon = icon("bar-chart")),
         menuItem("Crime Information", tabName = "crimeMap", icon = icon("map")),
         menuItem("Street Map", tabName = "streetMap", icon = icon("road"))
@@ -53,131 +51,13 @@ shinyUI(
       
       tabItems(
         
-        # Display Dashboard
-        
-        # tabItem(tabName = "dashboard",
-        #         
-        #         h2("Syracuse Civic Data Dashboard"),
-        #         
-        #         br(),
-        #         
-        #         h2("Pothole Information"),
-        #         
-        #         fluidRow(
-        #           
-        #           br(),
-        #           
-        #           valueBoxOutput("progressBox1a"),
-        #           
-        #           valueBoxOutput("progressBox1b"),
-        #           
-        #           valueBoxOutput("progressBox1c")
-        #         ),
-        #         
-        #         h2("Street Information"),
-        #         
-        #         fluidRow(
-        #           
-        #           br(),
-        #           
-        #           valueBoxOutput("progressBox2a"),
-        #           
-        #           valueBoxOutput("progressBox2b"),
-        #           
-        #           valueBoxOutput("progressBox2c")
-        #         )
-        # 
-        # ),
+        # Display overall rating interactive map
         
         tabItem(tabName = "overallsummary",
 
-                # Display Pothole Graphs and Widgets
-
                 fluidRow(
-                  # box(status = "warning",
-                  # 
-                  #     br(),
-                  #     uiOutput("o.year"),
-                  # 
-                  #   br(),
-                  # 
-                  #   selectInput(
-                  #     "month",
-                  #     "Select Month",
-                  #     choices = c(
-                  #       "January" = 1,
-                  #       "February" = 2,
-                  #       "March" = 3,
-                  #       "April" = 4,
-                  #       "May" = 5,
-                  #       "June" = 6,
-                  #       "July" = 7,
-                  #       "August" = 8,
-                  #       "September" = 9,
-                  #       "October" = 10,
-                  #       "November" = 11,
-                  #       "December" = 12
-                  #     ),
-                  #     selected = 5
-                  #   ),
-                  # 
-                  #   width = 4
-                  # ),
-                  # box(status = "warning",
-                  #     uiOutput("ranking"),
-                  #   plotOutput("o.bar1"),
-                  #   width = 8
-                  # )
-                  
-                  # title = "Recommendations",
-                  # id = "tabset", 
-                  # height = "250px",
-                  # 
-                  # tabPanel(
-                  #   "Interactive Map", 
-                  #   leafletOutput("map3")
-                  # ),
-                  # 
-                  # # tabPanel(
-                  # #   "Data Explorer"
-                  # #   DT::dataTableOutput("table.df")
-                  # # ),
-                  # width = 12
                   
                   tabName = "streetMap",
-                  # box(status = "warning",
-                  #     
-                  #     # selectInput(
-                  #     #   "road.attribute",
-                  #     #   "Road Attribute",
-                  #     #   choices = c(
-                  #     #     "Healthcare" = "healthcare",
-                  #     #     "Retail Stores" = "retail",
-                  #     #     "Road Rating" = "road"
-                  #     #   ),
-                  #     #   selected = "healthcare"
-                  #     # ),
-                  #     
-                  #     # br(),
-                  #     # 
-                  #     # uiOutput("o.year2"),
-                  #     # 
-                  #     # br(),
-                  #     # 
-                  #     # uiOutput("o.oil"),
-                  #     # 
-                  #     # br(),
-                  #     # 
-                  #     # uiOutput("o.class"),
-                  #     # 
-                  #     # br(),
-                  #     # 
-                  #     # uiOutput("o.pavement"),
-                  #     
-                  #     title = "Overall Ratings",
-                  #     width = 3
-                  #     
-                  # ),
                   
                   tabBox(
                     title = "Overall Recommendation",
@@ -189,30 +69,13 @@ shinyUI(
                       leafletOutput("map3")
                     ),
                     
-                    # tabPanel(
-                    #   "Data Explorer",
-                    #   DT::dataTableOutput("table.df2")
-                    # ),
                     width = 12
                   )
 
                 )
-
-                # fluidRow(
-                #   column(4,
-                #          br()
-                #          ),
-                # 
-                #   box(status = "warning",
-                #       uiOutput("o.street.abb2"),
-                #       plotOutput("o.bar2"),
-                #       width = 8
-                #   )
-                # )
-
         ),
         
-        # Display Pothole Maps and Widgets
+        # Display crime maps and widgets
         
         tabItem(tabName = "crimeMap",
                 
@@ -233,27 +96,6 @@ shinyUI(
                       ),
                       br(),
                       
-                      # uiOutput("o.street.abb"),
-                      # br(),
-                      # 
-                      # selectInput(
-                      #   "bus.type", 
-                      #   "Durapatcher Truck",
-                      #   choices = c(
-                      #     "Both",
-                      #     "DP1",
-                      #     "DP2"
-                      #   ),
-                      #   selected = "Both"
-                      # ),
-                      # br(),
-                      # 
-                      # dateRangeInput(
-                      #   'fix.date',
-                      #   label = 'Fix Date',
-                      #   start = Sys.Date() - 180, 
-                      #   end = Sys.Date()
-                      # ),
                       width = 3
                       
                   ),
@@ -267,16 +109,12 @@ shinyUI(
                       leafletOutput("map")
                     ),
                     
-                    # tabPanel(
-                    #   "Data Explorer"
-                    #   DT::dataTableOutput("table.df")
-                    # ),
                     width = 9
                   )
                 )
         ),
         
-        # Display Street Maps and Widgets
+        # Display street map for healthcare proximity, number of retail stores and road ratings
         
         tabItem(tabName = "streetMap",
                 fluidRow(
@@ -293,22 +131,6 @@ shinyUI(
                         selected = "healthcare"
                       ),
 
-                      # br(),
-                      # 
-                      # uiOutput("o.year2"),
-                      # 
-                      # br(),
-                      # 
-                      # uiOutput("o.oil"),
-                      # 
-                      # br(),
-                      # 
-                      # uiOutput("o.class"),
-                      # 
-                      # br(),
-                      # 
-                      # uiOutput("o.pavement"),
-
                       width = 3
 
                   ),
@@ -323,10 +145,6 @@ shinyUI(
                       leafletOutput("map2")
                     ),
 
-                    # tabPanel(
-                    #   "Data Explorer",
-                    #   DT::dataTableOutput("table.df2")
-                    # ),
                     width = 9
                   )
                 )
